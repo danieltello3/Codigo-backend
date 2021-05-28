fcfc45cfb90f09f00c2490c42e9957f416c99e08
Daniel Tello <dantello.319@gmail.com>
2021-05-27 22:06:02 -0500

    base de datos mysql, joins

---
+++ dia4/DB_empresa.sql
@@ -0,0 +1,24 @@
+create database if not exists empresa;
+use empresa;
+
+create table if not exists empleados(
+	id int not null unique primary key auto_increment,
+	nombre varchar(50),
+    apellido varchar(50),
+    identificador int,
+    departamento_id int,
+    supervisor_id int,
+    constraint relacion_departamento_empleado foreign key (departamento_id) references departamentos(id),
+    constraint relacion_supervisor_empleado foreign key (supervisor_id) references empleados(id)
+    );
+create table if not exists departamentos(
+	id int not null auto_increment primary key,
+	nombre varchar(50),
+    nivel int
+    );
+    
+INSERT INTO departamentos (nombre, nivel)VALUES 
+                         ('Ventas',1),
+                            ('Administracion',2),
+                            ('Finanzas',2),
+                            ('Marketing',3);
