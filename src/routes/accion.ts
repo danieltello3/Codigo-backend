0165ebdd7f76af224506f44fb43aca1e44f6077b
Daniel Tello <dantello.319@gmail.com>
2021-07-14 20:25:49 -0500

    limpieza de rama

--- src/routes/accion.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { accionRequestDto } from "../controllers/dto.request";
-import { crearAccion } from "../controllers/accion";
-
-export const accionRouter = Router();
-
-accionRouter.route("/acciones").post(accionRequestDto, crearAccion);
